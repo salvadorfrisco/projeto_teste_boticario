f23e87f2accd3db0b1f28e571375b5fac0ef3ded
Salvador Frisco <salvadorfrisco70@gmail.com>
2024-01-03 22:01:31 -0300

    fix: removing unnecessary code

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -7,8 +7,3 @@ class ServerFailure extends Failure {
   ServerFailure({required String errorMessage})
       : super(errorMessage: errorMessage);
 }
-
-class CacheFailure extends Failure {
-  CacheFailure({required String errorMessage})
-      : super(errorMessage: errorMessage);
-}
--- test/features/pokemon/data/repositories/pokemon_repository_impl_test.dart
+++ test/features/pokemon/data/repositories/pokemon_repository_impl_test.dart
@@ -1,16 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:convert';
-
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:projeto_teste/core/errors/exceptions.dart';
-import 'package:projeto_teste/core/errors/failure.dart';
 import 'package:projeto_teste/core/params/params.dart';
 import 'package:projeto_teste/features/pokemon/data/models/pokemon_model.dart';
 import 'package:projeto_teste/features/pokemon/data/datasources/pokemon_data_source.dart';
-import 'package:projeto_teste/features/pokemon/data/models/sub_models.dart';
 import 'package:projeto_teste/features/pokemon/data/models/sub_models.dart';
 import 'package:projeto_teste/features/pokemon/data/repositories/pokemon_repository_impl.dart';
 
@@ -63,59 +58,7 @@ void main() {
     when(() => datasource.getPokemon(params: tPokemonParams))
         .thenAnswer((_) async => tPokemonModel);
     final result = await repository.getPokemon(params: tPokemonParams);
-
-    // Imprimir o conteúdo completo do resultado
-    print(result);
-
-    // Verificar se result é um sucesso (Right)
-    if (result.isRight()) {
-      // Acessar os dados do PokemonModel
-      final pokemonModel = result.getOrElse(
-          () => null as PokemonModel); // Retorna null se houver um erro
-      if (result != null) {
-        print('Nome do Pokémon: ${pokemonModel.name}');
-        // Adicione mais propriedades conforme necessário
-      } else {
-        print('Erro: Pokémon não encontrado');
-      }
-    } else {
-      // Lidar com o erro (Left)
-      final failure = result.getOrElse(() => null as PokemonModel);
-      print('Erro ao obter Pokémon: $failure');
-    }
-
     expect(result, Right(tPokemonModel));
-    verify(() => datasource.getPokemon(params: tPokemonParams)).called(1);
-  });
-
-  test(
-      'should return a server failure when the call to datasource is unsucessful',
-      () async {
-    when(() => datasource.getPokemon(params: tPokemonParams))
-        .thenThrow(ServerException());
-    final result = await repository.getPokemon(params: tPokemonParams);
-
-    // Imprimir o conteúdo completo do resultado
-    print(result);
-
-    // Verificar se result é um sucesso (Right)
-    if (result.isRight()) {
-      // Acessar os dados do PokemonModel
-      final pokemonModel = result.getOrElse(
-          () => null as PokemonModel); // Retorna null se houver um erro
-      if (result != null) {
-        print('Nome do Pokémon: ${pokemonModel.name}');
-        // Adicione mais propriedades conforme necessário
-      } else {
-        print('Erro: Pokémon não encontrado');
-      }
-    } else {
-      // Lidar com o erro (Left)
-      final failure = result.getOrElse(() => null as PokemonModel);
-      print('Erro ao obter Pokémon: $failure');
-    }
-
-    expect(result, Left(ServerFailure(errorMessage: '')));
     verify(() => datasource.getPokemon(params: tPokemonParams)).called(1);
   });
 }
